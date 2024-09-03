fdd01116aa77d6458086e155da0d2ad35bf4c46e
Magnumja <162545876+Magnumja@users.noreply.github.com>
2024-09-02 21:28:12 -0400

    Update aula13.py

--- aula13.py
+++ aula13.py
@@ -3,7 +3,7 @@ altura = 1.80
 peso = 88
 imc = peso / altura ** 2
 
-
+# F-String 
 linha_1 = f'{nome} tem {altura:.2f} de altura,'
 linha_2 = f'pesa {peso} quilos e seu imc é'
 linha_3 = f'{imc:.2f}'
