03485e0942546bdc8fead9820a7ce33c16cf5ef9
Magnumja <162545876+Magnumja@users.noreply.github.com>
2025-03-17 21:45:45 -0400

    up

---
+++ aula15.py
@@ -0,0 +1,17 @@
+#ex 1
+# nome = input('Qual o seu nome? ') #sempre no tipo string
+# print(f'O seu nome eh {nome}')
+
+#numero_1 = input('Digite um numero: ')
+#numero_2 = input('Digite outro numero: ')
+
+#print(f'A soma dos numeros eh: {numero_1 + numero_2}') #aqui vai geerar uma concatenacao
+
+
+numero_1 = (input('Digite um numero: '))
+numero_2 = (input('Digite outro numero: '))
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma dos numeros eh: {numero_1 + numero_2}')
